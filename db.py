3c553ed9ea62bb3dca82362dc380680aae5572d3
KisuCoder <KisuCoder@users.noreply.github.com>
2026-02-13 22:08:06 +0800

    crud subject

---
+++ db.py
@@ -0,0 +1,8 @@
+db_config = {
+    'host': 'localhost',
+    'database': 'Info_Man_Proj',
+    'user': 'root',        # Change if your MySQL has different username
+    'password': 'SQL1234',        # Add your MySQL password
+    'port': 3306
+}
+
